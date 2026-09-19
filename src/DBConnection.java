5e3de760d252d440a3a3803aea1b1428a66f97df
V.Ramashanmugi <ramashanmugi@gmail.com>
2026-09-19 06:35:00 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String USER = "root";
 
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "shanmugi@123";
 
     public static Connection getConnection() {
 
